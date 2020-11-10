ae62143a8a28a6664b6dcbdc3fa5c9c17b0965d0
Rubenscode <rubens.andrade1@gmail.com>
2020-11-10 12:47:03 -0300

    Criando Model/Entity Categoria com atributos

---
+++ minhaLojaDeGames/src/main/java/br/org/generation/minhaLojaDeGames/model/Categoria.java
@@ -0,0 +1,60 @@
+package br.org.generation.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String nome;
+	
+	private boolean frete_gratis;
+
+	private boolean disponibilidade;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isFrete_gratis() {
+		return frete_gratis;
+	}
+
+	public void setFrete_gratis(boolean frete_gratis) {
+		this.frete_gratis = frete_gratis;
+	}
+
+	public boolean isDisponibilidade() {
+		return disponibilidade;
+	}
+
+	public void setDisponibilidade(boolean disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+	
+}
